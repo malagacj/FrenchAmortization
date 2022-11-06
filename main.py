251d77c9579d93244bb386df6f9ec2cf8fd84ce1
Jose Froylan Malaga Cardoza <plokoon1987@gmail.com>
2022-11-06 17:12:27 +0100

    Mejora al programa. Ahora multiplica por dos!!!!

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-loan = input("Por favor introduce el valor de tu prestamo:\n")
+loan = input('Por favor introduce el valor de tu prestamo: ')
+loan = int(loan)
+
+print(f'El prestamo introducido es {loan}')
+
+loan_x2 = loan*2
+
+print(f'Si doblas el prestamo sería {loan_x2}')
