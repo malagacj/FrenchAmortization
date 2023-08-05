060ef4093b3e56cba14b7874e80bb52b7a7082dd
Jose Froylan Malaga Cardoza <plokoon1987@gmail.com>
2023-08-05 16:11:45 +0200

    Initial finance files

---
+++ finance/test_tools.py
@@ -0,0 +1,20 @@
+from finance.tools import capitalization_ratio, french_amortization_fee
+
+
+def test_capitalization_ratio():
+    assert capitalization_ratio(1, 1) == 0.5
+    assert capitalization_ratio(3, 1) == 0.25
+    assert capitalization_ratio(4, 1) == 0.2
+    assert capitalization_ratio(7, 1) == 0.125
+    assert capitalization_ratio(9, 1) == 0.1
+    assert capitalization_ratio(3, 2) == 0.0625
+    assert capitalization_ratio(4, 2) == 0.04
+    assert capitalization_ratio(4, 3) == 0.008
+    assert capitalization_ratio(9, 3) == 0.001
+    assert capitalization_ratio(1, 4) == 0.0625
+    assert capitalization_ratio(4, 4) == 0.0016
+
+
+def test_french_amortization_fee():
+    assert french_amortization_fee(1_000_000, 1, 1) == 2_000_000
+    assert french_amortization_fee(1_000_000, 0.01, 1) == 1_010_000
